eae29550da40ba470bfc870ee87b5f9c79cfcf26
Nandana <nandana@Nandanas-MacBook-Air.local>
2023-08-19 15:21:01 +0530

    first commit

---
+++ src/components/About/index.js
@@ -0,0 +1,77 @@
+import { useEffect, useState } from 'react'
+import {
+  faAws,
+  faGithub,
+  faJava,
+  faMicrosoft,
+  faPython,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './index.scss'
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useState(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm a very ambitious Software developer looking for a role in an
+            established IT company with the opportunity to work with the latest
+            technologies on challenging and diverse projects.
+          </p>
+          <p align="LEFT">
+            I'm quiet confident, naturally curious, and perpetually working on
+            improving my chops one design problem at a time.
+          </p>
+          <p>
+            If I need to define myself in one word that would be a tech-enthusiastic!!!
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faPython} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faJava} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faAws} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faMicrosoft} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGithub} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default About
